3ee1d30fd5c2f718dd56cc48b957d5abb71c3438
Joshua Greer <jjgreer@asu.edu>
2024-03-01 20:40:14 -0700

    template elective service 1

---
+++ CrimeDataService/CrimeDataService/Service1.svc.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+/*
+Joshua Greer 1218576515
+CSE 445 Assignment 3 part 1 (individual)
+2/25/2024
+Crime Data Service
+
+required service 18 crime data service
+http://localhost:55023/Service1.svc
+(local host, run in browser)
+ServiceReference6
+
+known bugs at bottom
+*/
+
+
+namespace CrimeDataService
+{
+    /*THE PLAN ----------------------------------------------------------------------------------------------------------
+    1) Use StateAbbr to get the agency from the api
+    2) parse the string to find city and get the "ori" number
+    3) use the ori number to get crime data from summarized/agencies/{ori}/{offense}?"
+    4) then we grab the number of crimes after "actual"
+
+
+    for example:
+    we look up show low, az
+    we use https://api.usa.gov/crime/fbi/cde/agency/byStateAbbr/AZ?API_KEY=VabgMzS6bvqtEsdBIfFz17CVOFSvDiWblcyoL2NX
+    this should give us a list of all the agencies in arizona
+    
+    the return format is:
+    {
+        "ori": "AZ0010100",
+        "agency_name": "Show Low",
+    ...
+    }
+    {
+    ...
+    }
+    ...
+    we parse the string for show low
+    when we find it we walk back to the last ','
+    then we walk back over the ' " ' and grab the 9 digit ori number
+    we use https://api.usa.gov/crime/fbi/cde/arrest/agency/AZ0010000/all?from=2020&to=2020&API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv
+    returns in format
+    {
+    ... [a bunch of categories people get arrested for]
+    ...
+    "data_year": 2020,
+    [crime type]: [number of arrests],
+    [crime type]: [number of arrests],
+    ...
+    ]
+    }
+    this gives us the number of arrests in different categories, parse to where they good data begins
+    take the number string after each ':'
+    add the total of each category to get the total number of arrests within the year
+    we display this number as crimes per year
+     */
+
+
+    public class Service1 : IService1
+    {
+        //variables
+        string ORIcode = "";
+
+
+        // we changed it from lattitude/longitude to city/stateAbbr because the given api's don't exist anymore
+        public async Task<string> crimedata(String city, String stateAbbr)
+        {
+            //method variables
+            int backtrackLength = 17+9;// + city.Length + 9;
+            
+            //abrivation check
+            if(stateAbbr.Length != 2)
+            {
+                return "State abbreviation must be 2 characters";
+            }
+            else
+            {
+                stateAbbr = stateAbbr.ToUpper();
+            }
+            
+           //create http instance
+            using(HttpClient client = new HttpClient())
+            {
+                try
+                {
+
+                    //endpoint url
+                    string apiURL = "https://api.usa.gov/crime/fbi/cde/";
+                    string subURL = "/agency/byStateAbbr/" + stateAbbr + "?API_KEY=";
+                    string key = "VabgMzS6bvqtEsdBIfFz17CVOFSvDiWblcyoL2NX";
+
+                    string requestURL = apiURL + subURL + key;
+
+                    //make get request
+                    HttpResponseMessage response = await client.GetAsync(requestURL);
+
+                    //check for success
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //read content as string
+                        string jsonData = await response.Content.ReadAsStringAsync();
+
+                        string partial = jsonData.Substring(0,40000);
+
+                        //time to parse the data and get the ori code
+                        int index = partial.IndexOf(city);
+                        if(index == -1)
+                        {
+                            return "failed to find city in state";
+                        }
+                        else
+                        {
+                            ORIcode = partial.Substring(index - backtrackLength, 9);
+                            //we got the ori code and can use it to make our next request===================
+                            return ORIcode;
+                        }
+
+                    }
+                    else
+                    {
+                        return "failed to fetch data \nmake sure state abbreviation is valid (2 uppercase letters), and that city name is a city in the state\nMay error if city does not have a police department";
+                    }
+
+                }
+                catch (HttpRequestException ex)
+                {
+                    return "Error: " + ex.Message;
+                }
+
+            }
+
+        }
+        //==================================================================================================
+        public async Task<string> crimeCount(string ORI)
+        {
+            int sum = 0;
+            
+            //create http instance
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    //we are using the most recent year for info (2020) for our API Request
+                    string apiURL = "https://api.usa.gov/crime/fbi/cde/";
+                    string subURL = "arrest/agency/" + ORI + "/all?from=2020&to=2020&API_KEY=";
+                    string key = "VabgMzS6bvqtEsdBIfFz17CVOFSvDiWblcyoL2NX";
+
+                    string requestURL = apiURL + subURL + key;
+
+                    //make get request
+                    HttpResponseMessage response = await client.GetAsync(requestURL);
+                    
+                    //check for success
+                    if (response.IsSuccessStatusCode)
+                    {
+                        
+                        //read content as string
+                        string jsonData = await response.Content.ReadAsStringAsync();
+
+                        string partial = jsonData.Substring(0, 1355);
+
+                        //we have the data, now we need to pull the numbers and add them up
+                        int index = partial.IndexOf("2020");
+                        if (index == -1)
+                        {
+                            return "failed to calculate number of crimes";
+                        }
+                        else
+                        {
+                            string modified = partial.Substring(index);
+                            //now we want to get all integers that follow ':' characters, and add them to the sum
+                            //when we reach the "']' we know we are finished
+                            for (int i = 0; i < modified.Length; i++)
+                            {
+                                if (modified[i] == ':')
+                                {
+                                    int j = i + 1;
+                                    string num = "";
+                                    while (modified[j] != ',' && modified[j] != ']')
+                                    {
+                                        num += modified[j];
+                                        j++;
+                                    }
+                                    sum += int.Parse(num);
+                                }
+                            }
+                        }
+                        return "There is an average of " + sum + " crimes annually in this area.";
+
+                    }
+                    else
+                    {
+                        return "too large of database for my free api key, too much crime in the area";
+                    }
+
+                }
+                catch (HttpRequestException ex)
+                {
+                    return "Error: " + ex.Message;
+                }
+            }
+
+        }
+        //---------------
+
+    }
+    
+}
+
+/*
+Note*
+
+Some cities have police departments with custom names,
+some have databases too large for my public key to access (im limited to 65000 character requests)
+some cities have no police department at all
+some cities have police departments that are not in the database
+
+These cause errors from time to time, tho the program is pretty robust
+
+Test cases:
+Input:                  Output:
+Tempe AZ                5516
+Mesa AZ                 11063
+Show Low AZ             1145
+Apache AZ               32
+Brighton CO             1290
+Jourdanton TX           121
+Albany NY               224
+ */
---
+++ CrimeDataService/NaturalHazardsService/Service1.svc.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+/*
+Joshua Greer 1218576515
+CSE 445 Assignment 3 part 1 (individual)
+2/25/2024
+Natural Hazards Service
+required service 19 natural hazards service
+
+modified to be specifically for earthquakes because the API has global data
+http://localhost:56759/Service1.svc
+(open in browser before running tryIT local host)
+
+api info at https://earthquake.usgs.gov/fdsnws/event/1/
+known bugs at bottom
+*/
+
+namespace NaturalHazardsService
+{
+    /*THE PLAN================================================================================================
+    easier than last time
+    collect inputs
+    use the inputs to make a get request to the api
+    parse the json response for "count" of earthquakes
+    return the count as a string
+     */ 
+    public class Service1 : IService1
+     {
+        public async Task<string> NaturalHazards(double latitude, double longitude, int radiusKm, decimal minMag)
+        {
+            //we are actually going to do specifically earthquakes because this API has global data
+            int eqCount = 0;
+
+            //create http instance
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    //endpoint url
+                    string apiURL = "https://earthquake.usgs.gov/fdsnws/event/1/";
+                    //query string, we will show # of earthquakes over a year
+                    string subURL = "query?format=geojson&starttime=2022-01-01&endtime=2022-01-31&minmagnitude="+ minMag +
+                        "&latitude=" + latitude + "&longitude=" + longitude + "&maxradiuskm=" + radiusKm;
+
+                    string requestURL = apiURL + subURL;      //full request url
+
+                    //make get request
+                    HttpResponseMessage response = await client.GetAsync(requestURL);
+
+                    //check for success
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //read content as string
+                        string jsonData = await response.Content.ReadAsStringAsync();
+
+                        string partial = jsonData.Substring(0, 330);
+
+                        //now that we have partial data, we can parse it for the count of earthquakes
+                        int index = partial.IndexOf("count");
+                        if (index == -1)
+                        {
+                            return "failed to find the count of earthquakes";
+                        }
+                        else
+                        {
+                            int j = index + 7; //skip to the number
+                            string num = "";
+                            while (partial[j] != ',' && partial[j] != '}')
+                            {
+                                num += partial[j];
+                                j++;
+                            }
+                            eqCount += int.Parse(num);
+                            // 
+                            return "There has been " + eqCount + " earthquakes of this description in the last year";
+                        }
+
+                    }
+                    else
+                    {
+                        return "unsuccessful request (no data)";
+                    }
+
+                }
+                catch (HttpRequestException ex)
+                {
+                    return "unsuccessful request (no data)";
+                }
+            }
+        }
+     }
+}
+
+/*
+ Note*
+ MOST ERRORS ARE CUZ THE DATA IS NOT THERE
+
+ The API server does not like when you do small radius searches, it will return an error message
+ The API server does not like when you do large magnitude, it will return an error message
+ 
+ Most successful with magnitude 3.0 and radius 1000 
+
+
+Test Cases:
+Input (lat,long, radius, minMag) -> Expected Output (count of earthquakes)
+
+30 -110 1000 3.0 -> 41
+30 30 1000 3.0 -> 11
+50 50 1000 2.0 -> 1
+50 50 500 2.0 -> error (there are none)
+50 50 1000 3.0 -> 1
+ */
